243b6b7f59fc38c8d34d6951ed8e5ae286c21728
VitorGH <vitorbarrosor@gmail.com>
2024-11-14 01:28:57 -0300

    post to create task

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -5,13 +5,32 @@ const authenticateToken = require('../middleware/auth');
 
 router.get('/show', authenticateToken, async (req, res) => {
     try {
-        console.log('req.user', req.user)
         const ownerId = req.user.id
-        console.log('ownerId', ownerId)
         const task = await taskService.getUserTasks(ownerId);
-        res.json(task)
+        res.status(200).json(task)
     } catch (error) {
         res.status(500).json({ error: error.message });
+    }
+})
+
+router.post('/create', authenticateToken, async (req, res) => {
+    try {
+        const { title, description, status } = req.body;
+        const ownerId = req.user.id
+        const newTask = await taskService.createTask(title, description, status, ownerId);
+        res.status(201).json(newTask)
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+})
+
+router.delete('/delete', authenticateToken, async (req, res) => {
+    try {
+        const ownerId = req.user.id
+        const task = await taskService.getUserTasks(ownerId);
+        res.status(205).json(task)
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 })
 
